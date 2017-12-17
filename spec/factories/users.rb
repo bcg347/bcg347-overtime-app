db78b1121ab5164bafd4ccca7d0a47fa1b632320
Brandon Gordon <bgordon@mail.usf.edu>
2017-12-17 21:49:55 +0000

    Implemented tests and page redirection for permission structure

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -18,4 +18,12 @@ FactoryGirl.define do
       password 'asasass'
       password_confirmation 'asasass'
   end
+  
+  factory :non_authorized_user, class: "User"  do
+      first_name 'Non'
+      last_name 'Authorized'
+      email { generate :email}
+      password 'asasass'
+      password_confirmation 'asasass'
+  end
 end
