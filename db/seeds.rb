07c7f8d65981f45e1abd17c9a5942db0b52700d1
Brandon Gordon <bgordon@mail.usf.edu>
2017-12-27 02:21:25 +0000

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,13 @@ AdminUser.create(email: 'admin@test.com',
 
 puts "1 Admin User created"
 
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+
+puts "3 audit logs have been created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, 
   consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
@@ -27,9 +34,3 @@ end
 
 puts "100 Posts have been created"
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-
-
-puts "3 audit logs have been created"
